f5dc5a9c873c3f8e66c895ce0f83f7f70106574c
Dean Sofer <dean@Deans-MacBook-Pro.local>
2015-02-27 16:16:28 -0800

    Working on homepage

--- Down/www/js/controllers.js
+++ Down/www/js/controllers.js
@@ -1,12 +1,35 @@
 angular.module('downForIt.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope, Chats, $ionicActionSheet) {
+  $scope.chats = Chats.all();
+
+  // Triggered on a button click, or some other target
+  $scope.menu = function() {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: '<i class="icon ion-heart"></i> Subscribe to tag' },
+       { text: '<i class="icon ion-share"></i> Share this' },
+       { text: '<i class="icon ion-volume-mute"></i> Mute this type' }
+     ],
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+  };
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
-  }
+  };
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {

0e61f63db61f2342c45b210f8eab1a715c3aafbb
Dean Sofer <dean@Deans-MacBook-Pro.local>
2015-03-01 02:54:07 -0800

    Removed superfluous items on home

--- Down/www/js/home.js
+++ Down/www/js/home.js
@@ -1,6 +1,6 @@
 angular.module('downForIt.controllers')
 
-.controller('HomeCtrl', function($scope, TwitterLib, $ionicActionSheet, $state, $stateParams) {
+.controller('HomeCtrl', function($scope, TwitterLib, $state, $stateParams) {
 
   $scope.tag = $stateParams.tag;
 
@@ -59,24 +59,6 @@ angular.module('downForIt.controllers')
   };
 
   $scope.refresh();
-
-  $scope.menu = function() {
-   // Show the action sheet
-   var hideSheet = $ionicActionSheet.show({
-     buttons: [
-       { text: '<i class="icon ion-heart"></i> Subscribe to tag' },
-       { text: '<i class="icon ion-share"></i> Share this' },
-       { text: '<i class="icon ion-volume-mute"></i> Mute this type' }
-     ],
-     cancelText: 'Cancel',
-     cancel: function() {
-          // add cancel code..
-        },
-     buttonClicked: function(index) {
-       return true;
-     }
-   });
-  };
 
 })
 

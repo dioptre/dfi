a46bff681604ef58a05dfa51022f464493224721
Dean Sofer <dean@Deans-MacBook-Pro.local>
2015-03-01 10:41:53 -0800

    Added optional fake chat

--- Down/www/js/app.js
+++ Down/www/js/app.js
@@ -187,14 +187,18 @@ $stateProvider
     url: '/chats/:chatId',
     resolve: {
       event: function(TwitterLib, $stateParams) {
-        return TwitterLib.verify().then(function(res){
-          return TwitterLib.apiGetCall({
-            url: 'https://api.twitter.com/1.1/statuses/show.json',
-            data: {
-              id: $stateParams.chatId
-            }
+        if (window.cordova)
+          return TwitterLib.verify().then(function(res){
+            return TwitterLib.apiGetCall({
+              url: 'https://api.twitter.com/1.1/statuses/show.json',
+              data: {
+                id: $stateParams.chatId
+              }
+            });
           });
-        });
+        return {
+          id: $stateParams.chatId
+        };
       }
     },
     views: {

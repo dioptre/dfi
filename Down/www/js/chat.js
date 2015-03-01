b4e6c4e437f5ec596ad9efb05d11d196aee20a3a
Dean Sofer <dean@Deans-MacBook-Pro.local>
2015-02-28 22:12:14 -0800

    Wiring up chat page

--- Down/www/js/chat.js
+++ Down/www/js/chat.js
@@ -1,13 +1,18 @@
 angular.module('downForIt.controllers')
 
 .controller('ChatDetailCtrl', function($scope, Chats, TwitterLib, $stateParams, $firebase) {
-  var ref = new Firebase("https://downforit.firebaseio.com/events");
-  // create an AngularFire reference to the data
-  var sync = $firebase(ref.child($stateParams.chatId));
-  // download the data into a local object
-  var syncObject = sync.$asObject();
 
-  syncObject.$bindTo($scope, "event").then(function(){
+  var user = null;
+
+  TwitterLib.apiGetCall({
+    url: 'https://api.twitter.com/1.1/account/verify_credentials.json'
+  }).then(function(response){
+    user = response;
+  });
+  var ref = new Firebase("https://downforit.firebaseio.com/events/"+$stateParams.chatId);
+
+  var syncEvent = $firebase(ref).$asObject();
+  syncEvent.$bindTo($scope, "event").then(function(){
     // BOUND, data exists!
     // $scope.chats = {};
     if (!$scope.event.chatroom) {
@@ -15,9 +20,8 @@ angular.module('downForIt.controllers')
     }
   });
 
-  var chatSync = sync.child('chatroom').$asArray();
-
-
+  var syncMessages = $firebase(ref.child('chatroom').orderByChild('created_at DESC')).$asArray();
+  $scope.messages = syncMessages;
 
   var options = {
     url: "https://api.twitter.com/1.1/statuses/user_timeline.json",
@@ -29,6 +33,7 @@ angular.module('downForIt.controllers')
   
   $scope.message = function() {
     $scope.newMessage.created_at = new Date().getTime();
+    $scope.newMessage.sender = user;
     $scope.event.chatroom[new Date().getTime()] = $scope.newMessage;
     $scope.reset();
   };
@@ -36,7 +41,6 @@ angular.module('downForIt.controllers')
   $scope.reset = function() {
     $scope.newMessage = {
       text: '',
-      sender: '@proloser'
     };
   };
 
